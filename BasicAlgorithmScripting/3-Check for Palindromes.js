806d6beb98a00dbe9fd4eb60935dc6b0d5be7991
Artila <529276960@qq.com>
2017-04-15 01:33:42 +0800

    Rename

--- BasicAlgorithmScripting/3-Check for Palindromes.js
+++ BasicAlgorithmScripting/3-Check for Palindromes.js
@@ -4,10 +4,15 @@
  *  注意：需要去掉字符串多余的标点符号和空格，然后把字符串转化成小写来验证此字符串是否为回文。
  */
 function palindrome(str) {
-  // Good luck!
+  // 把非单词，下划线去掉
   var temp = str.replace(/[\W_]/g,'').toLowerCase();
+  // 和反转后的单词比较
   return temp === temp.split('').reverse().join('');
 }
 
 
-palindrome("eye");
+palindrome("eye"); // true
+palindrome("race car"); // true
+palindrome("My age is 0, 0 si ega ym."); // true
+palindrome("0_0 (: /-\ :) 0-0"); // true
+palindrome("1 eye for of 1 eye."); // false
